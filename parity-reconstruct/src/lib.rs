ad5de0cbf03bdcff5e6c8863ed66e014ac2baa42
David Venhoek <david@tweedegolf.com>
2024-06-05 10:25:54 +0200

    Made everything no_std, using bitvec for arrays of booleans.

--- parity-reconstruct/src/lib.rs
+++ parity-reconstruct/src/lib.rs
@@ -1,18 +1,33 @@
-use core::iter::repeat;
+//! Reconstruction algorithm for data from parity data.
+//! 
+//! Below, we use N to indicate the number of blocks in the data
+//! L is used to indicate the maximum number of missing data block
+//! that we are prepared to handle
+//! 
+//! We use U and V for BitArray backing storages of size sufficient
+//! to handle at least N bits (U) or L bits (V). This can't be done
+//! directly due to limitations in the rust type system.
+#![no_std]
+
+use core::marker::PhantomData;
+
+use bitvec::{array::BitArray, view::BitViewSized};
 
 /// Trait describing a parity matrix.
-pub trait ParityMatrix {
+pub trait ParityMatrix<U: BitViewSized> {
     /// Get the mth row of the parity matrix.
     ///
     /// All rows should be of exactly length N.
     ///
     /// For m < N, this should be a row with exactly
     /// one true, in position m.
-    fn row(&self, m: usize) -> Vec<bool>;
+    /// 
+    /// Any excess bits in the bitarray MUST be false
+    fn row(&self, m: usize) -> BitArray<U>;
 }
 
 /// Storage trait for matrix data
-pub trait MatrixStorage {
+pub trait MatrixStorage<V: BitViewSized> {
     /// Store a row of the parity reconsturction matrix.
     ///
     /// Called at most once for each value of m
@@ -20,12 +35,12 @@ pub trait MatrixStorage {
     /// When called for m, data has the following guarantees
     ///  data[m] = true
     ///  data[i] = false for all i > m
-    fn set_row(&mut self, m: usize, data: Vec<bool>);
+    fn set_row(&mut self, m: usize, data: BitArray<V>);
     /// Get a row of the parity reconsturction matrix.
     ///
     /// Note: can be called before corresponding set_row call.
     /// in that case, the result should be all false.
-    fn row(&self, m: usize) -> Vec<bool>;
+    fn row(&self, m: usize) -> BitArray<V>;
 }
 
 /// Storage trait for parity blocks
@@ -44,49 +59,52 @@ pub trait ParityStorage<const BLOCKSIZE: usize> {
 /// Storage trait for reconstructed data.
 pub trait DataStorage<const BLOCKSIZE: usize> {
     /// Store a newly received or reconstructed block.
-
+    ///
     /// Called exactly once for each value of m in 0..N
     fn store(&mut self, m: usize, data: [u8; BLOCKSIZE]);
     /// Get a block previously received.
-
+    ///
     /// Called only after a corresponding store call
     /// for m.
     fn get(&self, m: usize) -> [u8; BLOCKSIZE];
 }
 
 #[derive(Debug, Clone)]
-pub struct Reconstructor<Parity, Matrix, ParityData, Data, const BLOCKSIZE: usize> {
+pub struct Reconstructor<Parity, Matrix, ParityData, Data, const BLOCKSIZE: usize, U: BitViewSized, V: BitViewSized> {
+    n: usize,
     parity: Parity,
     matrix: Matrix,
     parityblocks: ParityData,
     datablocks: Data,
-    used: Vec<bool>,
+    used: BitArray<V>,
+    _phantom: PhantomData<U>
 }
 
 impl<
-        Parity: ParityMatrix,
-        Matrix: MatrixStorage,
+        Parity: ParityMatrix<U>,
+        Matrix: MatrixStorage<U>,
         ParityData: ParityStorage<BLOCKSIZE>,
         Data: DataStorage<BLOCKSIZE>,
         const BLOCKSIZE: usize,
-    > Reconstructor<Parity, Matrix, ParityData, Data, BLOCKSIZE>
+        U: BitViewSized,
+    > Reconstructor<Parity, Matrix, ParityData, Data, BLOCKSIZE, U, U>
 {
-    pub fn new(parity: Parity, matrix: Matrix, parityblocks: ParityData, datablocks: Data) -> Self {
-        let n = parity.row(0).len();
+    pub fn new(n: usize, parity: Parity, matrix: Matrix, parityblocks: ParityData, datablocks: Data) -> Self {
         Reconstructor {
+            n,
             parity,
             matrix,
             parityblocks,
             datablocks,
-            used: repeat(false).take(n).collect(),
+            used: BitArray::ZERO,
+            _phantom: PhantomData,
         }
     }
 
     // Returns true when we have sufficient data for reconstruction
     pub fn handle_block(&mut self, index: usize, mut data: [u8; BLOCKSIZE]) -> bool {
         let mut row = self.parity.row(index);
-        debug_assert_eq!(row.len(), self.used.len());
-        let mut working_head = row.len() - 1;
+        let mut working_head = self.n - 1;
         loop {
             if row[working_head] && self.used[working_head] {
                 // eliminate
@@ -94,15 +112,12 @@ impl<
                 for (i, el) in data.iter_mut().enumerate() {
                     *el ^= other_block[i];
                 }
-                let matrix_row = self.matrix.row(working_head);
-                for (i, el) in row.iter_mut().enumerate() {
-                    *el = *el != matrix_row[i];
-                }
+                row ^= self.matrix.row(working_head);
             } else if row[working_head] {
                 // new row, store it
                 self.matrix.set_row(working_head, row);
                 self.parityblocks.store(working_head, data);
-                self.used[working_head] = true;
+                self.used.set(working_head, true);
                 break;
             }
 
@@ -114,8 +129,8 @@ impl<
         }
 
         // Check whether we have sufficient data
-        if !self.used.iter().any(|v| !v) {
-            for i in 0..self.used.len() {
+        if !self.used.iter().take(self.n).any(|v| !v) {
+            for i in 0..self.n {
                 let mut output = self.parityblocks.get(i);
                 let matrix_row = self.matrix.row(i);
                 for j in 0..i {
@@ -137,40 +152,45 @@ impl<
 
 #[cfg(test)]
 mod tests {
+    extern crate std;
+
+    use core::iter::repeat;
+    use std::vec::Vec;
+
     use super::*;
 
     struct TestParity<const N: usize>;
 
-    impl<const N: usize> ParityMatrix for TestParity<N> {
-        fn row(&self, m: usize) -> Vec<bool> {
+    impl<const N: usize, U: BitViewSized> ParityMatrix<U> for TestParity<N> {
+        fn row(&self, m: usize) -> BitArray<U> {
             if m < N {
-                let mut out: Vec<_> = repeat(false).take(N).collect();
-                out[m] = true;
+                let mut out = BitArray::ZERO;
+                out.set(m, true);
                 out
             } else {
-                let mut out = Vec::with_capacity(N);
+                let mut out = BitArray::ZERO;
                 for i in 0..N {
-                    out.push((m - N) & 1_usize.checked_shl(i as _).unwrap_or(0) != 0);
+                    out.set(i, (m - N) & 1_usize.checked_shl(i as _).unwrap_or(0) != 0);
                 }
                 out
             }
         }
     }
 
-    struct TestMatrixStorage {
-        data: Vec<Vec<bool>>,
+    struct TestMatrixStorage<V: BitViewSized> {
+        data: Vec<BitArray<V>>,
     }
 
-    impl TestMatrixStorage {
+    impl<V: BitViewSized> TestMatrixStorage<V> {
         fn new(n: usize) -> Self {
             Self {
-                data: repeat(repeat(false).take(n).collect()).take(n).collect(),
+                data: repeat(BitArray::ZERO).take(n).collect(),
             }
         }
     }
 
-    impl MatrixStorage for TestMatrixStorage {
-        fn set_row(&mut self, m: usize, data: Vec<bool>) {
+    impl<V: BitViewSized> MatrixStorage<V> for TestMatrixStorage<V> {
+        fn set_row(&mut self, m: usize, data: BitArray<V>) {
             assert!(data[m]);
             for (i, el) in data.iter().enumerate() {
                 assert!(i <= m || !el)
@@ -178,7 +198,7 @@ mod tests {
             self.data[m] = data;
         }
 
-        fn row(&self, m: usize) -> Vec<bool> {
+        fn row(&self, m: usize) -> BitArray<V> {
             self.data[m].clone()
         }
     }
@@ -226,8 +246,9 @@ mod tests {
     #[test]
     fn simple_reconstruction_test() {
         let mut rec = Reconstructor::new(
+            4,
             TestParity::<4>,
-            TestMatrixStorage::new(4),
+            TestMatrixStorage::<[u8;1]>::new(4),
             BlockStorage::new(4),
             BlockStorage::new(4),
         );
@@ -244,8 +265,9 @@ mod tests {
     #[test]
     fn nontrivial_relation() {
         let mut rec = Reconstructor::new(
+            4,
             TestParity::<4>,
-            TestMatrixStorage::new(4),
+            TestMatrixStorage::<[u8;1]>::new(4),
             BlockStorage::new(4),
             BlockStorage::new(4),
         );
